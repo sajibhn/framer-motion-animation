b489d8a2b8500e248ea7bd75d6de27a501f5e57f
Sajib Hossain <sajibhossain1185@gmail.com>
2022-08-05 17:14:19 +0600

    animation on parent and children

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Box1 from './components/Box1/Box1'
 import Box2 from './components/Box2/Box2'
+import Box3 from './components/Box3/Box3'
 
 const App = () => {
   return (
     <div className='App'>
       <Box1 />
       <Box2 />
+      <Box3 />
     </div>
   )
 }
---
+++ src/components/Box3/Box3.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+
+const Box3 = () => {
+
+    const boxVariant = {
+        hidden: {
+            x: "-100vw",
+        },
+        visible: {
+            x: 0,
+            transition: {
+                delay: 0.5,
+                when: "beforeChildren",
+                staggerChildren: 0.2
+            }
+        }
+    }
+
+    const listVariant = {
+        hidden: {
+            x: -10,
+            opacity: 0,
+        },
+        visible: {
+            x: 0,
+            opacity: 1,
+
+        }
+    }
+
+
+
+    return (
+        <div className='box-container'>
+            <motion.div className="box"
+                variants={boxVariant}
+                animate="visible"
+                initial="hidden"
+            >
+                {[1, 2, 3].map((box) => {
+                    return (
+                        <motion.li
+                            variants={listVariant}
+                            className='boxItem'> </motion.li>
+                    )
+                })}
+            </motion.div>
+        </div>
+    )
+}
+
+export default Box3
