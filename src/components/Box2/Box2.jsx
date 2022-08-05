b977ed3709ad09b6de0598dc57da1823d97ce51f
Sajib Hossain <sajibhossain1185@gmail.com>
2022-08-05 16:35:58 +0600

    dragging animation

--- src/components/Box2/Box2.jsx
+++ src/components/Box2/Box2.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const Box2 = () => {
     return (
-        <div>Box2</div>
+        <div className='box-container'>
+            <motion.div className="box"
+                drag
+                dragConstraints={{
+                    right: 20,
+                    left: 0,
+                    top: 20,
+                    bottom: 20
+                }}
+                whileHover={{
+                    scale: 1.1
+                }}
+                whileTap={{
+                    scale: 0.9
+                }}
+            >
+            </motion.div>
+        </div>
     )
 }
 
