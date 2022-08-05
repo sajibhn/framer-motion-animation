3063d5b4d51a8aae52a837db14f3c8c3c449e21a
Sajib Hossain <sajibhossain1185@gmail.com>
2022-08-05 17:44:24 +0600

    animation using useAnimate hook

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Box1 from './components/Box1/Box1'
 import Box2 from './components/Box2/Box2'
 import Box3 from './components/Box3/Box3'
 import Box4 from './components/Box4/Box4'
+import Box5 from './components/Box5/Box5'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Box2 />
       <Box3 />
       <Box4 />
+      <Box5 />
     </div>
   )
 }
