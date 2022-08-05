01cd034568b265cb9e3477820666e476999aefcf
Sajib Hossain <sajibhossain1185@gmail.com>
2022-08-05 17:23:56 +0600

    animation using array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Box1 from './components/Box1/Box1'
 import Box2 from './components/Box2/Box2'
 import Box3 from './components/Box3/Box3'
+import Box4 from './components/Box4/Box4'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Box1 />
       <Box2 />
       <Box3 />
+      <Box4 />
     </div>
   )
 }
